8e0c4c8b3fa93fb126bc4016fd41ccedbce8c183
Roshan <rshnrasal@gmail.com>
2021-07-20 20:28:29 +0530

    'Added-some-js'

--- Userproject/settings.py
+++ Userproject/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-32=p%3utl9#utdr^p1slz1dm*oqo5gps7hm3rds-e1_)gl&y^+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1']
 
