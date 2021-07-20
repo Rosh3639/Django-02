e782b9a24cd7073fd1454feef84bcee6e881f593
Roshan <rshnrasal@gmail.com>
2021-07-20 21:51:44 +0530

    'Added-some-js'

--- Userproject/settings.py
+++ Userproject/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-32=p%3utl9#utdr^p1slz1dm*oqo5gps7hm3rds-e1_)gl&y^+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1']
 
@@ -115,6 +115,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'home/static'),
+]
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
 
 
 # Default primary key field type
--- home/urls.py
+++ home/urls.py
@@ -3,6 +3,9 @@ from django.urls import path
 from home import views
 from django.contrib.auth import views as auth_views
 from home.views import EmailValidationOnForgotPassword
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('', views.index, name="home"),
     path('login', views.loginUser, name="login"),
@@ -19,4 +22,4 @@ urlpatterns = [
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name="registration/password_reset_confirm.html"), name="password_reset_confirm"),
     path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name="registration/password_reset_complete.html"), name="password_reset_complete"),
 
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
