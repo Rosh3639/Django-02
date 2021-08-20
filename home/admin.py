84d1317f562cf2d888c1c3ad65f9e00257f7b728
Roshan <rshnrasal@gmail.com>
2021-08-20 18:49:29 +0530

    Added new function to download/expport data ffrom database

--- home/admin.py
+++ home/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 from .models import *
+from import_export.admin import ImportExportModelAdmin
+
+
 # Register your models here.
 
-admin.site.register(Users)
+@admin.register(Users)
+class UserDetail(ImportExportModelAdmin):
+    pass
